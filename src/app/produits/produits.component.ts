3391e4bff51c24b4f18ccc82307b2f1e6123997f
unknown <bmaryam@ept.sn>
2024-10-18 18:56:56 +0200

    groo

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -18,7 +18,26 @@ products: any[] = []; // Array to hold the list of products
   ngOnInit(): void {
     this.loadProducts();
   }
+ // Method to get the status in French
+ getStatusLabel(status: string): string {
+  switch (status) {
+    case 'draft':
+      return 'Brouillon';
+    case 'approved':
+      return 'En ligne';
+    case 'pending':
+      return 'En attente';
+    case 'rejected':
+      return 'Rejeté';
+    default:
+      return 'Inconnu';
+  }
+}
 
+// Method to join categories with 'et'
+getCategoriesString(categories: any[]): string {
+  return categories && categories.length > 0 ? categories.map(cat => cat.name).join(' et ') : 'Aucune catégorie';
+}
   loadProducts(): void {
     const accessToken = localStorage.getItem('loggedInUserToken');
 
